18a345eee9b63a8654904d7951bca1da81b704bd
victormusembi11 <victor.musembi11@gmail.com>
2023-09-02 16:57:32 +0300

    fetch all posts using oop Post Manager class

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 
+require __DIR__ . "/db_conn.php";
+
 include_once __DIR__ . "/crud.php";
+include_once __DIR__ . "/crud_oop.php";
 
 /* Fetch all Posts */
-$posts = fetch_all_posts($pdo);
+// $posts = fetch_all_posts($pdo);
 
-foreach ($posts['data'] as $post) {
-    echo $post->title . "<br>";
-}
+// foreach ($posts['data'] as $post) {
+//     echo $post->title . "<br>";
+// }
 
 /* Create Post */
 // $post = create_post(
@@ -35,3 +38,18 @@ foreach ($posts['data'] as $post) {
 // foreach ($search_results["data"] as $post) {
 //     echo $post->title . "<br>";
 // }
+
+/* Fetch All Posts OOP */
+$postManager = new PostManager($pdo);
+
+$result = $postManager->fetchAllPosts();
+
+if ($result["status"]) {
+    $posts = $result["data"];
+
+    foreach ($posts as $post) {
+        echo $post->title . "<br>";
+    }
+} else {
+    echo "Error: " . $result["message"];
+}
